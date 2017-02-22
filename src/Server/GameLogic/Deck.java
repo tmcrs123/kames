118da14ad09fd80e9b5dc35f79e4175a48322281
Peter <oliveia.ss.pedro@gmail.com>
2017-02-22 22:18:38 +0000

    class Deck: corrected removeCard() to pollCard() - removed addCard() & getRandomCard() (no use for them)

--- src/Server/GameLogic/Deck.java
+++ src/Server/GameLogic/Deck.java
@@ -8,18 +8,18 @@ import java.util.LinkedList;
  */
 public class Deck {
 
+
     public static final String[] POSSIBLE_CARD_VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
     private LinkedList<Card> cards;
+
 
     public Deck() {
         cards = new LinkedList<>();
         initCards();
     }
 
-
     private void initCards() {
-
         //CREATES 4 CARDS OF EACH VALUE
         for (String iValue : POSSIBLE_CARD_VALUES) {
             for (int i = 0; i < 4; i++) {
@@ -29,33 +29,17 @@ public class Deck {
         Collections.shuffle(cards);
     }
 
-
-
     public Card showLastCard() {
         return cards.getLast();
     }
 
-    public Card getRandomCard() {
-
-        int rand = (int) (Math.random()*cards.size());
-        return cards.get(rand);
+    public Card pollCard() {
+        return cards.poll();
     }
-
-    public void addCard(Card card) {
-        cards.add(card);
-    }
-
-    public void removeCard() {
-        cards.remove();
-    }
-
 
     public void give4CardsTo(Hand hand) {
-
         for (int givenCards = 0; givenCards < 4; givenCards++) {
-            hand.receiveCard(cards.poll());
+            hand.receiveCard(pollCard());
         }
     }
-
-
 }
