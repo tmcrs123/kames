2e1280503a77b2da49d0f525aa015458fa9ea093
Joaquim T. Duarte <joaquimtd.info@gmail.com>
2017-02-22 18:37:42 +0000

    Started: ChatServer and ClientHandler

--- src/Server/Chat/Chat.java
+++ src/Server/Chat/Chat.java
@@ -2,9 +2,10 @@ package Server.Chat;
 
 import Server.GameLogic.Game;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -23,44 +24,88 @@ public class Chat {
      */
 
 
-    private static final int PORT_NUMBER = 8080;
-    private static final String HOST = "localhost";
+    private static final int PORT_NUMBER = 8080;                   // Port to listen for connections
+    private static final String HOST = "localhost";                // Host = 127.0.0.1, runs on localhost
 
-    private Game game;
-    private List<ClientHandler> clients;
-    private boolean gameStarted;
+    private Game game;                                             // Game object that will have all logic
+    private List<ClientHandler> clients;                           // List of Client threads
+    private boolean gameStarted;                                   // If game has started = true
+    private static final int PLAYER_LIMIT = 4;                     // Limit number of players
+    private int numOfConnections;                                  // Number of players
 
 
     public void start(){
         try {
+
+            System.out.println("Starting server at port: " + PORT_NUMBER + ", please wait...");
             ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-            Socket clientSocket = serverSocket.accept();
+            System.out.println("Server started: " + serverSocket);
 
-            //n meto o resto pk o Joaquim disse que queria ver a parte do server. Up to you
+            while(numOfConnections < PLAYER_LIMIT) {
 
+                try {
 
+                // Waits for client connections
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostName());
+
+                    // Creates a new ClientHandler
+                    numOfConnections++;
+                    String name = "Player-" + numOfConnections;
+                    ClientHandler client = new ClientHandler(clientSocket, this);
+                    clients.add(client);
+
+                    // Serves the client connection with a new thread
+                    Thread thread = new Thread(client);
+                    thread.setName(name);
+                    thread.start();
+
+                } catch(IOException ex) {
+                    System.out.println("Error receiving the client connection: " + ex.getMessage());
+                }
+            }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Unable to start port at: " + PORT_NUMBER);
         }
     }
 
+    /**
+     * Checks the message for commands, if it is a command it will
+     * start with a "/". If not it will send a chat message.
+     * @param message
+     */
+    public void validateMessage(String nickname, String message){
 
+        if(message.startsWith("/")) {
+            runGameCommand(nickname, message);
 
-    public void runGameCommand(String message){
+        } else if(gameStarted) {
+            broadcast(nickname, message);
+
+        } else {
+            sendMessageToPartner(nickname, message);
+        }
+    }
+
+    public void runGameCommand(String nickname, String message){
         throw new UnsupportedOperationException();
     }
 
-    public void validateMessage(String message){
+    public void sendMessageToPartner(String nickname, String message){
         throw new UnsupportedOperationException();
     }
 
-    public void sendMessageToPartner(String message){
-        throw new UnsupportedOperationException();
-    }
+    public void broadcast(String nickname, String message){
 
-    public void broadcast(String message){
-        throw new UnsupportedOperationException();
+        synchronized (clients) {
+
+            Iterator<ClientHandler> it = clients.iterator();
+
+            while(it.hasNext()) {
+                it.next().send(nickname, message);
+            }
+        }
     }
 
 
